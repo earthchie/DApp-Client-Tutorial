4821dc6b3f8208603546a90452cc6f14de601aa9
Thanarat Kuawattanaphan <earthchie@gmail.com>
2023-01-02 19:25:49 +0700

    Update index.js

--- 03 Check Token Balance/index.js
+++ 03 Check Token Balance/index.js
@@ -2,17 +2,17 @@ const {ethers} = require('ethers');
 
 async function app(){
 
-    // BSC RPC list: https://docs.binance.org/smart-chain/developer/rpc.html
-    const provider = new ethers.providers.JsonRpcProvider('https://bsc-dataseed.binance.org/');
+    // RPC list: https://chainlist.org/
+    const provider = new ethers.providers.JsonRpcProvider('https://rpc-l1.jibchain.net');
     
-    const BUSD_Contract = new ethers.Contract('0xe9e7cea3dedca5984780bafc599bd69add087d56', [
+    const Token = new ethers.Contract('0xF2Bd9c13E5c65aEDb48CfAfa806e4996c40D2f20', [
         'function balanceOf(address account) external view returns (uint256)'
     ], provider);
 
     const walletAddress = '0x9999999d46618d114C133e8746396c5bF0090b2B';
-    const BUSD_balance = await BUSD_Contract.balanceOf(walletAddress);
+    const balance = await Token.balanceOf(walletAddress);
 
-    console.log('My BUSD Balance is:', ethers.utils.formatUnits(BUSD_balance));
+    console.log('My balance is:', ethers.utils.formatUnits(balance));
 
 }
 
